74de8a57ec6a8177b3f58c85c6b79c5832c5e56d
Alex Matv <matveev.98.o@bk.ru>
2024-04-11 20:10:52 +0300

    Начальное состояние сервера и клиента (идентичное ЛР2)

---
+++ client.py
@@ -0,0 +1,98 @@
+import socket
+import os
+import pickle
+import time
+
+
+class File:
+    def __init__(self, name, directory):
+        self.name = name
+        self.directory = directory
+        self.data = self.read_data()
+
+    def read_data(self):
+        with open(os.path.join(self.directory, self.name)) as f:
+            return f.readlines()
+
+
+SERVER = "localhost"
+PORT = 9090
+
+TYPE = socket.AF_INET
+PROTOCOL = socket.SOCK_STREAM
+
+folder = "f_1"
+directory = os.path.join(os.getcwd(), folder)
+
+
+def create_directories(folder):
+    if not os.path.isdir(os.path.join(os.getcwd(), folder)):
+        os.mkdir(os.path.join(os.getcwd(), folder))
+
+def take_info(directory):
+    info = os.listdir(directory)
+    return info
+
+
+def create_file(data, directory):
+    with open(os.path.join(directory, data.name), "w") as f:
+        for line in data.data:
+            f.write(line)
+        print(f"Файл {data.name} создан")
+
+
+def remove_file(msg, directory):
+    name = msg.split()[1]
+    os.remove(os.path.join(directory, name))
+    print(f"Файл {name} удалён")
+
+
+def change_dirs(data, directory):
+    if data == "OK":
+        print("Принят запрос OK")
+        return
+    if type(data) == str:
+        remove_file(data, directory)
+        print("Удален файл")
+    else:
+        create_file(data, directory)
+
+
+create_directories(folder)
+
+while True:
+    try:
+        sock = socket.socket(TYPE, PROTOCOL)
+        sock.connect((SERVER, PORT))
+        msg = take_info(directory)
+        msg = pickle.dumps(msg)
+        sock.send(msg)
+
+        print("Состояние директории отправлено серверу")
+
+        recieved_data = b''
+        fl = False
+
+        while True:
+            if fl:
+                sock.settimeout(2.0)
+                try:
+                    data = sock.recv(1)
+                    recieved_data += data
+                except socket.timeout:
+                    break
+            else:
+                data = sock.recv(1)
+                fl = True
+                if not data:
+                    break
+                recieved_data += data
+
+        if recieved_data != b'':
+            recieved_data = pickle.loads(recieved_data)
+            change_dirs(recieved_data, directory)
+
+        time.sleep(15)
+    finally:
+        print("Closing connection.")
+        sock.close()
---
+++ server.py
@@ -0,0 +1,84 @@
+import socket
+import os
+import pickle
+import time
+
+
+HOST = ""
+PORT = 9090
+
+TYPE = socket.AF_INET
+PROTOCOL = socket.SOCK_STREAM
+
+folder = "f_main"
+directory = os.path.join(os.getcwd(), folder)
+
+
+def create_directories(folder):
+    if not os.path.isdir(os.path.join(os.getcwd(), folder)):
+        os.mkdir(os.path.join(os.getcwd(), folder))
+
+
+def check_directory(directory, dir_client):
+    dir_server = os.listdir(directory)
+    for f in dir_client:
+        if f not in dir_server:
+            print("Отправлен запрос на remove")
+            return f"remove {f}"
+
+    for f in dir_server:
+        if f not in dir_client:
+            print("Отправлен запрос на create")
+            return File(f, directory)
+    print("Отправлен запрос на OK")
+    return "OK"
+
+
+class File:
+    def __init__(self, name, directory):
+        self.name = name
+        self.directory = directory
+        self.data = self.read_data()
+
+    def read_data(self):
+        with open(os.path.join(self.directory, self.name)) as f:
+            return f.readlines()
+
+
+srv = socket.socket(TYPE, PROTOCOL)
+srv.bind((HOST, PORT))
+
+create_directories(folder)
+
+while 1:
+    srv.listen(1)
+    print("Слушаю порт 9090")
+    sock, addr = srv.accept()
+    print("Подключен клиент", addr)
+    recieved_data = b''
+    fl = False
+    try:
+        while True:
+            if fl:
+                sock.settimeout(2.0)
+                try:
+                    pal = sock.recv(1)
+                    recieved_data += pal
+                except socket.timeout:
+                    break
+            else:
+                pal = sock.recv(1)
+                fl = True
+                if not pal:
+                    break
+                recieved_data += pal
+
+        print(f"Получено от {addr}")
+        recieved_data = pickle.loads(recieved_data)
+        msg = check_directory(directory, recieved_data)
+        msg = pickle.dumps(msg)
+        sock.send(msg)
+        print("Запрос отправлен клиенту")
+    except ConnectionResetError:
+        print("Connection closed by client.")
+        sock.close()
